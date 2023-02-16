6af82cf0a91b74cbd39823cb9553a3edec23f101
CoronadoW <w_cicerone@hotmail.com>
2023-02-16 03:03:54 -0300

    Se agrego cards a experiencia y el navbar con los iconos de redes
    Se definieron las columnas para las card en experiencia.
    Se agrego <app-redes></app-redes> al codigo del navbar y se definio el margin top para esta etiqueta.
    Se quito el app-redes del componente header.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { LogoArgProComponent } from './componentes/logo-arg-pro/logo-arg-pro.com
 import { RedesComponent } from './componentes/redes/redes.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { AboutComponent } from './componentes/about/about.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import { AboutComponent } from './componentes/about/about.component';
     LogoArgProComponent,
     RedesComponent,
     BannerComponent,
-    AboutComponent
+    AboutComponent,
+    NavbarComponent,
+    ExperienciasComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
