08b5e3507e479f23a3cb57b4ee38a33864e713cd
CoronadoW <w_cicerone@hotmail.com>
2023-03-18 15:11:12 -0300

    About cambio imagen

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -1,12 +1,12 @@
 export class persona {
     id?: number;
     nombre: String;
-    apellido: String;
+    titulo: String;
     img: String;
 
-    constructor(nombre: String, apellido: String, img: String) {
+    constructor(nombre: String, titulo: String, img: String) {
         this.nombre = nombre;
-        this.apellido = apellido;
+        this.titulo = titulo;
         this.img = img;
     }
 }
