bfb02b7d4891572bca6df7a71db8dc89281d71b7
CoronadoW <w_cicerone@hotmail.com>
2023-05-13 00:55:50 -0300

    login casi completo con firebase
    login realizado con firebase, falta darle un toque a los redireccionamientos y en que vista arrancaria la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { InicioComponent } from './componentes/inicio/inicio.component';
 import { PortfolioComponent } from './componentes/portfolio/portfolio.component';
 import { SkillsComponent } from './componentes/skills/skills.component';
 import { RegisterComponent } from './componentes/register/register.component';
-import { canActivate, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 /*para bloquear ciertas rutas si estamos logueados con firebase o no importamos canActivate, y de esta manera si no estamos registrados nos mandara al componente que se le indique
 para poder ir de un componente a otro y navegar en la misma pagina importamos el Router.
